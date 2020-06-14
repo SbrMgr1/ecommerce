3e0d2bff4f694436e9e764e92fa50195187bd482
sbr mgr <sbr.mgr1@gmail.com>
2020-06-14 18:07:06 -0500

    Managed Admin Login

--- src/main/java/servlets/Admins/Logout.java
+++ src/main/java/servlets/Admins/Logout.java
@@ -12,6 +12,6 @@ public class Logout extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getSession().invalidate();
-        resp.sendRedirect("/admin-login?msg=");
+        resp.sendRedirect("/login?msg=");
     }
 }
