933bcd3d12c17c46ad41ff602d4686c6f57978f5
pradipacharya7 <pradip.acharya077@gmail.com>
2020-06-15 14:03:56 -0500

    method added in listenor

--- src/main/java/controllers/Admins/cms/ContentManage.java
+++ src/main/java/controllers/Admins/cms/ContentManage.java
@@ -26,9 +26,9 @@ public class ContentManage extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        CMSDao cmsDao = (CMSDao)this.getServletContext().getAttribute("cmsDao");
-        List<CMS> cmss= cmsDao.getAllCMS();
-        req.setAttribute("cms",cmss);
+//        CMSDao cmsDao = (CMSDao)this.getServletContext().getAttribute("cmsDao");
+//        List<CMS> cmss= cmsDao.getAllCMS();
+//        req.setAttribute("cms",cmss);
        req.getRequestDispatcher("/WEB-INF/views/admins/cms.jsp").forward(req,resp);
     }
 
