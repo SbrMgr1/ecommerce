ccf12270842c150ddff26ab8dbce8dacb933d099
deep_bohara <dpak23bohara@gmail.com>
2020-06-15 01:11:15 -0500

    Category form to table still need to fix..

--- src/main/java/controllers/Admins/ProductCategory.java
+++ src/main/java/controllers/Admins/ProductCategory.java
@@ -1,8 +1,10 @@
 package controllers.Admins;
 
+import Dao.ProductCategoryDao;
 import com.google.gson.Gson;
 import models.CMS;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,21 +12,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 @WebServlet("/administration/product-cat")
 public class ProductCategory extends HttpServlet {
-    Gson mapper=new Gson();
+    private ProductCategoryDao dao;
+    Gson mapper = new Gson();
+
+    @Override
+    public void init() throws ServletException {
+        dao = new ProductCategoryDao();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<ProductCategory> categoryList = (List<ProductCategory>)this.getServletContext().getAttribute("productCategory");
+        req.setAttribute("categoryList",categoryList);
+
         req.getRequestDispatcher("/WEB-INF/views/admins/productCategory.jsp").forward(req,resp);
+
+
+
+
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String productCat=req.getParameter("pc");
+        String productCat = req.getParameter("productCategory");
         System.out.println(productCat);
-        ProductCategory pc=mapper.fromJson(productCat, ProductCategory.class);
+        ProductCategory productCategory = mapper.fromJson(productCat, ProductCategory.class);
         PrintWriter out = resp.getWriter();
-        out.print(mapper.toJson(pc));
+        out.print(mapper.toJson(productCategory));
+
+
     }
 }
--- src/main/java/models/ProductCategory.java
+++ src/main/java/models/ProductCategory.java
@@ -7,10 +7,25 @@ public class ProductCategory {
     private String name;
     private String desc;
 
-    public ProductCategory(String name, String desc) {
-        this.id = MyHelper.getRandomInt();
+    public ProductCategory(){
+      super();
+    }
+
+    public ProductCategory(int id,String name, String desc) {
+        super();
+        this.id = id;
         this.name = name;
         this.desc = desc;
+    }
+    public ProductCategory(String name, String desc) {
+        super();
+//        this.id = MyHelper.getRandomInt();
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public long getId() {
